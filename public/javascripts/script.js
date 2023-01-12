12c0d82969650398876f9ec5f806c0de91c7eb09
Abraham Toledo <abrahamtoledo90@gmail.com>
2023-01-12 12:23:05 +0100

    Frontend implementation with React

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,38 +1,69 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const filterComp = this.querySelector('[data-filter]');
+'use strict';
 
-  filterComp.addEventListener('change', function(e) {
-    const score = e.target.value;
-    console.log("hello")
+const {useState, useEffect} = React;
 
-    fetch(`attractions/search?score=${score}`)
-      .then(response => response.json())
-      .then(data => {
-        const contactsComp = this.querySelector('.attractions');
-        // data has a list of attractions with name and average_review_score
-        // render each in div with class attraction and inside two divs with class attraction-name and attraction-score respectively
-
-        // clear the list
-        contactsComp.innerHTML = '';
-
-        data.forEach(attraction => {
-          const attractionComp = document.createElement('div');
-          attractionComp.classList.add('attraction');
-
-          const nameComp = document.createElement('div');
-          nameComp.classList.add('attraction-name');
-          nameComp.innerText = attraction.name;
-
-          const scoreComp = document.createElement('div');
-          scoreComp.classList.add('attraction-score');
-          scoreComp.innerText = attraction.average_review_score;
-
-          attractionComp.appendChild(nameComp);
-          attractionComp.appendChild(scoreComp);
-
-          contactsComp.appendChild(attractionComp);
-        });
-      }
-    );
+const apiCall = async (url, options = {}) => {
+  const response = await fetch(url, options);
+  const json = await response.json();
+  if (response.ok) {
+    return json;
   }
-});
+  throw new Error(response.status);
+}
+
+const getAllAttractions = async () => await apiCall('/attractions/all');
+
+const getAttractionsByReviewScore = async (score) => await apiCall(`/attractions/search?score=${score}`);
+
+const getAttractions = async (score) => {
+  if (score && !isNaN(score) && score >= 1 && score <= 10) {
+    try {
+      return await getAttractionsByReviewScore(score);
+    } catch (err) {
+      // console.log(err);
+    }
+  }
+
+  return await getAllAttractions();
+}
+
+const Filter = ({onFilter}) => {
+  const [filter, setFilter] = useState('');
+
+  const handleChange = (e) => {
+    setFilter(e.target.value);
+    onFilter(e.target.value);
+  }
+
+  return (
+    <div className="filters">
+      <label htmlFor="data-filter">Show me things to do that have an average score higher than</label>
+      <input data-filter type="text" id="data-filter" value={filter} onChange={handleChange} />
+    </div>
+  );
+}
+
+const AttractionList = () => {
+  const [attractions, setAttractions] = useState([]);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    getAttractions(score).then(setAttractions);
+  }, [score]);
+
+  return (
+    <div>
+      <Filter onFilter={setScore} />
+      {attractions.map(attraction => (
+        <div key={attraction.id} className="attraction">
+          <div className="attraction-score">{attraction.average_review_score?.toFixed?.(1) ?? '-'}</div>
+          <div className="attraction-name">{attraction.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const container = document.querySelector('#react-attractions');
+const root = ReactDOM.createRoot(container);
+root.render(<AttractionList />);
